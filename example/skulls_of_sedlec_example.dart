651e6a4cc01c843d847f181c517e3feac0b02c29
emilio_com <jorgeemilio7777@gmail.com>
2025-12-01 08:13:33 -0700

    first commit

---
+++ example/skulls_of_sedlec_example.dart
@@ -0,0 +1,6 @@
+import 'package:skulls_of_sedlec/skulls_of_sedlec.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/skulls_of_sedlec_test.dart
@@ -0,0 +1,16 @@
+import 'package:skulls_of_sedlec/skulls_of_sedlec.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
